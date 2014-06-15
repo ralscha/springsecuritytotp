cea439f4f5eeef40f0d04c7e8096733e7f55cf3b
Ralph Schaer <ralphschaer@gmail.com>
2014-06-15 13:38:04 +0200

    Add home request mapping method

--- src/main/java/ch/rasc/sec/controller/HelloController.java
+++ src/main/java/ch/rasc/sec/controller/HelloController.java
@@ -1,5 +1,8 @@
 package ch.rasc.sec.controller;
 
+import java.util.Date;
+import java.util.Map;
+
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +14,19 @@ import ch.rasc.sec.security.JpaUserDetails;
 @Controller
 public class HelloController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/sayHello", method = RequestMethod.GET)
 	@ResponseBody
 	public String sayHello(@AuthenticationPrincipal JpaUserDetails user) {
 		return "Hello " + user.getUsername();
 	}
 
+	@RequestMapping("/")
+	public String home(Map<String, Object> model,
+			@AuthenticationPrincipal JpaUserDetails user) {
+		model.put("message", "Hello World");
+		model.put("title", "Hello Home");
+		model.put("date", new Date());
+		model.put("user", user.getUsername());
+		return "home";
+	}
 }
