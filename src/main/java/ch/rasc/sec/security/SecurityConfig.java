0e8f3a3518eae17d7db8ba393660eaa449f25bfe
Ralph Schaer <ralphschaer@gmail.com>
2014-06-15 13:39:29 +0200

    Add configuration for logout and the h2 console web page

--- src/main/java/ch/rasc/sec/security/SecurityConfig.java
+++ src/main/java/ch/rasc/sec/security/SecurityConfig.java
@@ -1,8 +1,12 @@
 package ch.rasc.sec.security;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,43 +15,79 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfig {
 
 	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+	protected WebMvcConfigurerAdapter viewControllers() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addViewControllers(ViewControllerRegistry registry) {
+				registry.addViewController("/login").setViewName("login");
+			}
+
+		};
 	}
 
-	@Autowired
-	public void configureGlobal(UserDetailsService userDetailsService, AuthenticationManagerBuilder auth)
-			throws Exception {
+	@Configuration
+	public static class DefaultWebSecurityConfigurerAdapter extends
+			WebSecurityConfigurerAdapter {
 
-		TotpAuthenticationConfigurer configurer = new TotpAuthenticationConfigurer(userDetailsService)
-				.passwordEncoder(passwordEncoder());
-		auth.apply(configurer);
+		@Bean
+		public PasswordEncoder passwordEncoder() {
+			return new BCryptPasswordEncoder();
+		}
+
+		@Autowired
+		public void configureGlobal(UserDetailsService userDetailsService,
+				AuthenticationManagerBuilder auth) throws Exception {
+
+			TotpAuthenticationConfigurer configurer = new TotpAuthenticationConfigurer(
+					userDetailsService).passwordEncoder(passwordEncoder());
+			auth.apply(configurer);
+		}
+
+		@Override
+		public void configure(WebSecurity builder) throws Exception {
+			builder.ignoring().antMatchers("/robots.txt");
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests()
+				.anyRequest()
+				.authenticated()
+			.and()
+				.formLogin()
+				.authenticationDetailsSource(new TotpWebAuthenticationDetailsSource())
+				.loginPage("/login").failureUrl("/login?error").permitAll()
+			.and()
+				.logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"));
+		}
 	}
 
-	@Override
-	public void configure(WebSecurity builder) throws Exception {
-		builder.ignoring().antMatchers("/robots.txt");
+	@Configuration
+	@Order(1)
+	public static class H2ConsoleSecurityConfigurationAdapter extends
+			WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.antMatcher("/console/**").authorizeRequests().anyRequest().fullyAuthenticated()
+					.and().csrf().disable().headers().disable();
+		}
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		//@formatter:off
-		http
-		  .authorizeRequests()
-		    .anyRequest().authenticated()
-		  .and()
-		  .formLogin()
-		    .authenticationDetailsSource(new TotpWebAuthenticationDetailsSource())
-		    .loginPage("/login.jsp")
-			.failureUrl("/login.jsp?error")
-			.permitAll();
-		//@formatter:on
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(
+				new WebServlet());
+		registration.addUrlMappings("/console/*");
+		return registration;
 	}
-
 }
