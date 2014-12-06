abaeeaffce47461bfd041529cc4897e567b411d9
Ralph Schaer <ralphschaer@gmail.com>
2014-12-06 18:23:33 +0100

    Upgrade to Spring Boot 1.2.0 RC2

--- src/main/java/ch/rasc/sec/Main.java
+++ src/main/java/ch/rasc/sec/Main.java
@@ -1,15 +1,11 @@
 package ch.rasc.sec;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Main extends SpringBootServletInitializer {
 
 	@Override
