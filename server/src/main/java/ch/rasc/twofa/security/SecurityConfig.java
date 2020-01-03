028bcbdba2eefda7be7f64f8d03d9494e03655a2
Ralph Schaer <ralphschaer@gmail.com>
2020-01-03 06:23:37 +0100

    Upgrade libraries

--- server/src/main/java/ch/rasc/twofa/security/SecurityConfig.java
+++ server/src/main/java/ch/rasc/twofa/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-		    .csrf().disable()
+		    .csrf(customizer -> customizer.disable())
 		    .authorizeRequests(customizer -> {
 		        customizer
 		          .antMatchers("/authenticate", "/signin", "/verify-totp",
